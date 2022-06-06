957b743c5e45cda356e038efbab61250aac7ca10
N-D-P <anshp9918@gmail.com>
2022-06-06 09:18:03 +0530

    PRODUCT ROUTE

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const UserRouter = require("./routes/user");
 const AuthRouter = require("./routes/auth");
+const ProductRouter = require("./routes/product");
 
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use("/api/users",UserRouter);
 app.use("/api/auth",AuthRouter);
+app.use("/api/products",ProductRouter);
 
 
 
